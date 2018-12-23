551a2031d74923536ee9e901b7cf3bfd35c9f767
adam <adamsandstrom1989@gmail.com>
2018-12-23 03:45:19 +0100

    main menu

--- projekt.sh
+++ projekt.sh
@@ -1,2 +1,38 @@
 #!/bin/bash
-echo "skafft skript"
+
+# FUNKTION - SKRIV UT BANNER
+banner() {
+	echo "#"
+}
+
+# FUNKTION - MENY
+menu() {
+	echo -e "group:add\t\tCreate a new user"
+	echo -e "group:list\t\tList system groups"
+	echo -e "group:view\t\tList user associations for group"
+	echo -e "group:modify\t\tModify user associations for group"
+	echo -e "user:add\t\tCreate a new user"
+	echo -e "user:list\t\tList system user"
+	echo -e "user:view\t\tView user properties"
+	echo -e "user:modify\t\tModify user properties"
+	echo -e "folder:add\t\tCreate a new folder"
+	echo -e "folder:list\t\tList folder contents"
+	echo -e "folder:view\t\tView folder properties"
+	echo -e "folder:modify\t\tModify folder properties"
+}
+
+# FUNKTION - MAIN
+main() {
+	# kalla på banner funktion
+	banner
+
+	# while loop tills avsluta
+	while true; do
+		# Skriv ut meny
+		menu
+
+	done
+
+}
+
+main
